9447eaf9df3654d597d8927488000963441395fe
Julio Pasinatto <julio.pasinatto@percona.com>
2024-07-25 18:03:33 -0300

    Improve example - comparing with bundle

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,43 @@
-import pytest
-import subprocess
 import os
-import logging
 import yaml
+import pytest
+import requests
+import logging
+import subprocess
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 NAMESPACE = "default-ns"
-VALUES_FILE = "values.yaml"
 K8S_VERSION = os.getenv("K8S_VERSION", "1.28.0")
 
 
-@pytest.fixture(scope="session")
-def helm_template(chart_path):
-    def _helm_template(release_name="test-deploy", values=None):
-        command = ['helm', 'template', release_name, chart_path, "--kube-version", K8S_VERSION, "--namespace", NAMESPACE]
-        if values:
-            command.extend(['--values', values])
+@pytest.fixture(scope="class")
+def helm_template():
+    def _helm_template(chart_path, release_name, values):
+        command = [
+            "helm",
+            "template",
+            release_name,
+            chart_path,
+            "--kube-version",
+            K8S_VERSION,
+            "--namespace",
+            NAMESPACE,
+            "--values",
+            values,
+        ]
         helm_template_output = subprocess.check_output(command)
-        return list(yaml.full_load_all(helm_template_output.decode('utf-8')))
+        return list(yaml.full_load_all(helm_template_output))
+
     return _helm_template
+
+
+@pytest.fixture(scope="class")
+def get_resources_from_bundle():
+    def _get_resources_from_bundle(operator_project_name, version):
+        full_url = f"https://raw.githubusercontent.com/percona/{operator_project_name}/v{version}/deploy/bundle.yaml"
+        response = requests.get(full_url)
+        return list(yaml.safe_load_all(response.text))
+
+    return _get_resources_from_bundle
